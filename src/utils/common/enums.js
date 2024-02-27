38e4b7743b497a63556f789c52a5161d4a110fae
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-02-27 18:03:14 +0530

    Added models for many to many associations between roles and user

--- src/utils/common/enums.js
+++ src/utils/common/enums.js
@@ -1,14 +1,9 @@
-const SEAT_TYPE = {
-  BUSINESS: "business",
-  ECONOMY: "economy",
-  PREMIUM_ECONOMY: "premium-economy",
-  FIRST_CLASS: "first-class",
+const USER_ROLE_ENUMS = {
+  ADMIN: "admin",
+  CUSTOMER: "customer",
+  FLIGHT_COMPANY: "flight_company",
 };
 
-const BOOKING_STATUS = {
-  BOOKED: "booked",
-  CANCELLED: "cancelled",
-  INITIATED: "initiated",
-  PENDING: "pending",
+module.exports = {
+  USER_ROLE_ENUMS,
 };
-module.exports = { SEAT_TYPE, BOOKING_STATUS };
